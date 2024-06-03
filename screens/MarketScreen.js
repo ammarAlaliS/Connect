6625947b8560a0d35d605ed73e55a8d29f0a9686
ammarAlaliS <ammaralialiwa@gmail.com>
2024-06-03 15:38:40 -0600

    se preparo el componente para ser trabajado

--- screens/MarketScreen.js
+++ screens/MarketScreen.js
@@ -1,13 +1,8 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import Header from '../components/Header';
-import { useDispatch } from 'react-redux';
-import { addUser } from '../globalState/userSlice';
+
 
 const MarketScreen = () => {
-
-    
-
     return (
         <View>
     
