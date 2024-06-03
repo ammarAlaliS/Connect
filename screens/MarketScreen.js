af2f679dc775f8bac3fdc0ee25724b06e0a00125
ammarAlaliS <ammaralialiwa@gmail.com>
2024-06-03 15:06:22 -0600

    se preparo el component market para ser utilizado

--- screens/MarketScreen.js
+++ screens/MarketScreen.js
@@ -5,17 +5,12 @@ import { useDispatch } from 'react-redux';
 import { addUser } from '../globalState/userSlice';
 
 const MarketScreen = () => {
-    const dispatch = useDispatch();
+
     
-    useEffect(() => {
-        fetch("https://jsonplaceholder.typicode.com/users/1")
-        .then((response)=> response.json())
-        .then((data) => dispatch(addUser(data)))
-    }, []) 
 
     return (
         <View>
-            <Header />
+    
         </View>
     );
 }
